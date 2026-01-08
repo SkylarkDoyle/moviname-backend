a3fc0a9bdf3fdecb10c75245cec309a75a797bba
Atobaru Adedoyin <atobaru@peepalytics.com>
2026-01-08 12:36:44 +0100

    fix: added new site to cors allowed origins

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Film Recognition API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://moviname.onrender.com"],
+    allow_origins=["http://localhost:5173", "https://moviname.onrender.com", "https://www.moviename.site"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
